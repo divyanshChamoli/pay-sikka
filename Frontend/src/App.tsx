b7902ee47715a4a7ec4eb1942c84635d77d4b1dc
Divyansh Chamoli <divyanshchamoli567@gmail.com>
2024-09-15 18:55:24 +0530

    added tailwind and routing to frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,22 @@
+import { BrowserRouter,Routes ,Route } from "react-router-dom"
+import Signin from "./Pages/Signin"
+import Signup from "./Pages/Signup"
+import Dashboard from "./Pages/Dashboard"
+import Send from "./Pages/Send"
 
 function App() {
-
+  
   return (
     <>  
       PaySikka
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/signin" element={<Signin/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/send" element={<Send/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
